a61859770d308b83d43e13d7c3d94144517a6581
Henrique <hassis.moreira177@hotmail.com>
2024-06-12 16:21:21 -0300

    DB conection | DTO Verification

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { EmblemasModule } from './emblemas/emblemas.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    EmblemasModule, UsersModule, AuthModule],
+    EmblemasModule, UsersModule, AuthModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,7 +1,23 @@
+import { IsOptional, IsUUID, MaxLength, MinLength, IsString, IsEmail, IsUrl } from "class-validator";
+
 export class UsersDto {
+    @IsUUID()
+    @IsOptional()
     id: string;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(30)
     username: string;
+
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @MinLength(8)
     password: string;
+
+    @IsUrl()
+    @IsOptional()
     pfp: string;
 }
